9c34955fbac5bbb186b46f41d9d26b31541a2235
mika tiihonen <koneautomaatio@hotmail.com>
2022-02-17 05:33:34 +0200

    css muutoksia+ otsikko lisätty

--- src/index.js
+++ src/index.js
@@ -108,7 +108,11 @@ class Game extends React.Component {
 
     return (
       <div className="game">
+        <div>
+          
+        </div>
         <div className="game-board">
+        <h1>Tic Tac Toe</h1>
           <Board 
             squares={current.squares}
             onClick={i => this.handleClick(i)}
